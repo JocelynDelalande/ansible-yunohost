b1bc54c1eaedfe2f67a158dc799eaba13e6efb18
Jocelyn Delande <jocelyn@crapouillou.net>
2015-11-19 00:44:09 +0100

    [fix] Helper args handling using subparser

--- ansible_ynh/helper.py
+++ ansible_ynh/helper.py
@@ -11,9 +11,20 @@ PLAYBOOK_PATH = '/usr/share/ansible-yunohost/playbooks/ynh-app-operation.yml'
 
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('operation')
-    parser.add_argument('app_name')
-    parser.add_argument('vars')
+    subparsers = parser.add_subparsers(dest='operation')
+
+    parser_install = subparsers.add_parser('install')
+    parser_upgrade = subparsers.add_parser('upgrade')
+    parser_remove = subparsers.add_parser('remove')
+
+    for i in parser_install, parser_upgrade, parser_remove:
+        i.add_argument('app_name')
+
+    parser_install.add_argument('vars')
+
+    for i in parser_upgrade, parser_remove:
+        parser_upgrade.set_defaults(vars=None)
+
     return parser.parse_args()
 
 
